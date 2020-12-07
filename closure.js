51c6da95caafdf64f54c4772d63ad09cb608d83c
Natalia Wit <natwit95@gmail.com>
2020-12-06 21:22:41 -0500

    solution for closure

---
+++ closure.js
@@ -0,0 +1,23 @@
+//what is a closure?
+//Code out an example closure that functions properly?
+
+
+const globalVariable = "global var";
+
+function outterFunc(param1) {
+	const variable1 = "var one";
+
+	function innerFunc(param2) {
+		const variable2 = "var two";
+
+		console.log("globalVariable: ", globalVariable);
+		console.log("variable1: ", variable1);
+		console.log("variable2: ", variable2);
+		console.log("param1: ", param1);
+		console.log("param2: ", param2);
+	}
+
+	innerFunc("param one");
+}
+
+outterFunc("param two");
