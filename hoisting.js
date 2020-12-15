14738d96260f814b6bc417175aeb11657ccd9a90
Natalia Wit <natwit95@gmail.com>
2020-12-14 20:51:43 -0500

    hoisting

--- hoisting.js
+++ hoisting.js
@@ -45,4 +45,53 @@ let globalVar = 'global'
         function getState() {
             return 'New York'
         }
-})()
+    })()
+
+
+    //PART 2
+
+    //Variables and functions are hoisted to the top of the scope that they are declared in
+    //whether that is a global scope, a function scope, or a block scope- depending on the keyword you use to define them (var, let const)
+
+    // tab 1
+ 
+function getTotal() {
+  console.log(multiplier);
+  console.log(total);
+  
+  let total = 0;
+  
+  for(var i = 0; i < 10; i++) {
+    
+    let valueToAdd = i;
+    var multiplier = 2;
+    total += valueToAdd * multiplier;
+  }
+  
+  return total;
+}
+ 
+getTotal();
+ 
+ 
+ 
+// tab 2
+ 
+function getTotal() {
+  let total;
+  var multiplier;
+  
+  total = 0;
+  
+  for(var i = 0; i < 10; i++) {
+    let valueToAdd;
+    
+    valueToAdd = i;
+    multiplier = 2;
+    total += valueToAdd * multiplier;
+  }
+  
+  return total;
+}
+ 
+getTotal();
